b013fceefc11888407944941f50e724ee34329a6
Drakizz31 <beaupere.damien31@gmail.com>
2025-10-06 21:08:55 +0200

    TP2: invariant pré/post conditions sur Romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,16 +3,20 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private boolean invariantVerifie() {
+		return force >= 0;
+	}
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-	}
+		assert invariantVerifie(): "Invariant violé après création : force négative";
+		}
 
 	public String getNom() {
 		return nom;
 	}
-
+	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
@@ -22,14 +26,23 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		//postcondition: stocker la force avant le coup pour la vérification
+		int forceAvantCoup = force;
+		//précondition 
+		assert forcecoup > 0 : "Précondition violé : la force du coup reçu doit être positive.";
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne !");
 		}
+		//Postcondition
+		assert force < forceAvantCoup : "Postcondition violée : La force du Romain n'a pas diminué.";
+		//Invariant
+		assert invariantVerifie(): "Invariant violé après création : force négative";
 	}
 	public static void main(String[] args) {
-		
+		Romain minus = new Romain("Minus", 6);
+		minus.parler(" Je suis prêt à combattre !");
 	}
 }
